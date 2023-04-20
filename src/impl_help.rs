a515c6baa3a5d4f5419c137a2b46a3a7f377c21b
Philipp A <flying-sheep@web.de>
2023-04-20 16:50:09 +0200

    Add constructor and getters to FontCharacteristics (#18)

--- src/impl_help.rs
+++ src/impl_help.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! impl_richcmp_eq {
-    ($cls: tt) => {
+    ($cls: path) => {
         #[cfg(feature = "pyo3")]
         #[pyo3::pymethods]
         impl $cls {
@@ -18,6 +18,28 @@ macro_rules! impl_richcmp_eq {
                     Ne => (self != other).into_py(py),
                     _ => py.NotImplemented(),
                 };
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_bitflags_accessors {
+    ($cls: path, $( $flag: ident ),+ $(,)?) => {
+        paste::paste! {
+            #[cfg(feature = "pyo3")]
+            #[pyo3::pymethods]
+            impl $cls {
+                $(
+                    #[getter]
+                    fn [< get_ $flag >](&self) -> bool {
+                        self.contains($cls::[< $flag:upper >])
+                    }
+                    #[setter]
+                    fn [< set_ $flag >](&mut self, value: bool) {
+                        self.set($cls::[< $flag:upper >], value);
+                    }
+                )+
             }
         }
     };
--- tests/test_draw.py
+++ tests/test_draw.py
@@ -10,3 +10,12 @@ def test_import_structure():
     assert isinstance(draw.Rgba, type)
     assert isinstance(draw.Style, type)
     assert isinstance(draw.Pen, type)
+
+
+def test_font_characteristics():
+    fc1 = draw.FontCharacteristics()
+    fc2 = draw.FontCharacteristics(bold=True)
+    assert fc1 != fc2
+    assert not fc1.bold
+    assert fc2.bold
+    assert repr(fc2) == "FontCharacteristics(BOLD)"
